bc46d7a50a4dcabc715da173111603bc324e3349
VladyBarvy <vladbarskoff@yandex.ru>
2025-03-25 22:18:15 +0300

    250325 deep modifi thermistor_color

--- thermistor_color.js
+++ thermistor_color.js
@@ -18,31 +18,31 @@ const app = () => {
     let input_color_1 = document.getElementById('color1');
     let select_input_color_1 = input_color_1.options[input_color_1.selectedIndex].value;
     if (select_input_color_1 === "Чёрный") {
-      data_1 = 1;
+      data_1 = 0;
     }
     if (select_input_color_1 === "Коричневый") {
-      data_1 = 2;
+      data_1 = 1;
     }
     if (select_input_color_1 === "Красный") {
-      data_1 = 3;
+      data_1 = 2;
     }
     if (select_input_color_1 === "Оранжевый") {
-      data_1 = 4;
+      data_1 = 3;
     }
     if (select_input_color_1 === "Жёлтый") {
-      data_1 = 5;
+      data_1 = 4;
     }
     if (select_input_color_1 === "Зелёный") {
-      data_1 = 6;
+      data_1 = 5;
     }
     if (select_input_color_1 === "Синий") {
-      data_1 = 7;
+      data_1 = 6;
     }
     if (select_input_color_1 === "Фиолетовый") {
-      data_1 = 8;
+      data_1 = 7;
     }
     if (select_input_color_1 === "Серый") {
-      data_1 = 9;
+      data_1 = 8;
     }
 
 
@@ -50,31 +50,31 @@ const app = () => {
     let input_color_2 = document.getElementById('color2');
     let select_input_color_2 = input_color_2.options[input_color_2.selectedIndex].value;
     if (select_input_color_2 === "Чёрный") {
-      data_2 = 1;
+      data_2 = 0;
     }
     if (select_input_color_2 === "Коричневый") {
-      data_2 = 2;
+      data_2 = 1;
     }
     if (select_input_color_2 === "Красный") {
-      data_2 = 3;
+      data_2 = 2;
     }
     if (select_input_color_2 === "Оранжевый") {
-      data_2 = 4;
+      data_2 = 3;
     }
     if (select_input_color_2 === "Жёлтый") {
-      data_2 = 5;
+      data_2 = 4;
     }
     if (select_input_color_2 === "Зелёный") {
-      data_2 = 6;
+      data_2 = 5;
     }
     if (select_input_color_2 === "Синий") {
-      data_2 = 7;
+      data_2 = 6;
     }
     if (select_input_color_2 === "Фиолетовый") {
-      data_2 = 8;
+      data_2 = 7;
     }
-    if (select_input_color_2 === "Белый") {
-      data_2 = 0;
+    if (select_input_color_2 === "Серый") {
+      data_2 = 8;
     }
 
 
@@ -83,9 +83,6 @@ const app = () => {
     let input_color_3 = document.getElementById('color3');
     let select_input_color_3 = input_color_3.options[input_color_3.selectedIndex].value;
     if (select_input_color_3 === "Золотой") {
-      data_3 = 0.01;
-    }
-    if (select_input_color_3 === "Серебряный") {
       data_3 = 0.1;
     }
     if (select_input_color_3 === "Чёрный") {
@@ -97,6 +94,12 @@ const app = () => {
     if (select_input_color_3 === "Красный") {
       data_3 = 100;
     }
+    if (select_input_color_3 === "Оранжевый") {
+      data_3 = 1000;
+    }
+    if (select_input_color_3 === "Жёлтый") {
+      data_3 = 10000;
+    }
 
 
 
@@ -104,9 +107,6 @@ const app = () => {
     let select_input_color_4 = input_color_4.options[input_color_4.selectedIndex].value;
     if (select_input_color_4 === "Золотой") {
       data_4 = ' ±5%';
-    }
-    if (select_input_color_4 === "Коричневый") {
-      data_4 = ' ±1%';
     }
     if (select_input_color_4 === "Красный") {
       data_4 = ' ±2%';
@@ -118,7 +118,7 @@ const app = () => {
 
     // Расчёт сопротивления
     //let resistance = (((data_1 * 10000) + (data_2 * 1000)) * data_3);
-    let resistance = (((data_1 * 10) + data_2) * 1000) * data_3;
+    let resistance = ((data_1 * 10) + data_2) * data_3;
 
     document.getElementById('resistance_show').value = resistance + `${data_4}`;
     //document.getElementById('resistance_show').textContent = resistance + `${data_4}`;
